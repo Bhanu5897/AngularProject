8e4ad6953ed7bc862f99e9853d79207989ff55de
Neerajsinghns <ns4029505@gmail.com>
2023-12-24 19:47:40 +0530

    uodate header-page

--- Angular-Project/src/app/app.component.ts
+++ Angular-Project/src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { TimelineModule } from 'primeng/timeline';
+import { CalendarModule } from 'primeng/calendar';
+import { HeaderComponent } from "./Website-home/header/header.component";
+import { DropdownModule } from 'primeng/dropdown';
+import { FooterComponent } from "./Website-home/footer/footer.component";
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, ButtonModule, TimelineModule, CalendarModule, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'Angular-Project';
+  date: Date | undefined;
 }
